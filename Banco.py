2efd0222c333a21ac571a335c415a21a37c80ef4
Daniel André Martins <danielandremartins.06@gmail.com>
2024-10-22 23:12:50 -0300

    Projeto Crud 02
    Atividade 02

---
+++ Banco.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+class Banco():
+    def __init__(self):
+        self.conexao = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "root",
+            database = "danielmartins_db"
+        )
+        self.create.Table()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS Usuario (id_usuario INT AUTO_INCREMENT PRIMARY KEY,   
+            nome TEXT ,
+            telefone TEXT ,
+            email TEXT ,
+            usuario TEXT ,
+            senha TEXT )''')
+        self.conexao.commit()
+        c.close()
---
+++ Usuarios.py
@@ -0,0 +1,39 @@
+from ast import Try
+import mysql.connector
+from Banco import Banco
+
+class Usuarios(object):
+    def __init__(self, id_usuario = 0, nome = "", telefone ="", email = "", usuario = "", senha = ""):
+        self.info = {}
+        self.id_usuario = id_usuario
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.usuario = usuario
+        self.senha = senha
+
+    def insertUser(self):
+        Banco = Banco()
+        try:
+            c= Banco.conexao.cursor()
+            c.execute("INSERT INTO usuario(nome, telefone, email, usuario, senha)VALUES(%s,%s,%s,%s,%s)", 
+                            (self.nome, self.telefone, self.email, self.usuario, self.senha))
+            Banco.conexao.commit()
+            c.close()
+            return "Usuario cadastrado com sucesso"
+
+        except Exception as e:
+            return f"Ocorreu um erro na inserção de usuarios: {e}"
+
+    def insertUser(self):
+        Banco = Banco()
+        try:
+            c= Banco.conexao.cursor()
+            c.execute("UPDATE FROM usuario WHERE(nome, telefone, email, usuario, senha)VALUES(%s,%s,%s,%s,%s)", 
+                            (self.nome, self.telefone, self.email, self.usuario, self.senha))
+            Banco.conexao.commit()
+            c.close()
+            return "Usuario atualizado com sucesso"
+
+        except Exception as e:
+            return f"Ocorreu um erro na atualização de usuarios: {e}"
